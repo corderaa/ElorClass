30e240ac57405ff46aa98ef927dd76f30f04a31e
Javi <in2dm3-v@elorrieta.int>
2025-01-08 19:36:46 +0100

    Corrección de la conexión a internet

--- app/src/main/java/com/example/elorclass/MainActivity.kt
+++ app/src/main/java/com/example/elorclass/MainActivity.kt
@@ -15,26 +15,6 @@ import com.example.elorclass.functionalities.Functionalities
 
 class MainActivity : AppCompatActivity() {
 
-    private val networkCallback = object : ConnectivityManager.NetworkCallback() {
-        // network is available for use
-        override fun onAvailable(network: Network) {
-            super.onAvailable(network)
-        }
-
-        // Network capabilities have changed for the network
-        override fun onCapabilitiesChanged(
-            network: Network,
-            networkCapabilities: NetworkCapabilities
-        ) {
-            super.onCapabilitiesChanged(network, networkCapabilities)
-            val unmetered = networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_NOT_METERED)
-        }
-
-        // lost network connection
-        override fun onLost(network: Network) {
-            super.onLost(network)
-        }
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -54,8 +34,6 @@ class MainActivity : AppCompatActivity() {
             .build()
 
         val connectivityManager = getSystemService(ConnectivityManager::class.java) as ConnectivityManager
-        connectivityManager.requestNetwork(networkRequest, networkCallback)
-
         val functionalities = Functionalities()
 
         buttonConnexion.setOnClickListener{
