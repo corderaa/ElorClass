bd6b13e5b3eb7abd56ec36d696e9c9d6b85044bc
SasoriAka96 <nahivallejo46@gmail.com>
2025-01-17 17:03:05 +0100

    clases para socket.io

---
+++ app/src/main/java/com/example/elorclass/socketIO/config/Events.kt
@@ -0,0 +1,13 @@
+package com.example.elorclass.socketIO.config
+
+enum class Events(val value: String) {
+    ON_LOGIN("onLogin"),
+    ON_GET_ALL("onGetAll"),
+    ON_RESPONSE_LOGIN("onLoginAnswer"),
+    ON_REGISTER("onRegister"),
+    ON_RESPONSE_REGISTER("onRegisterAnswer"),
+    TEST_EVENT("testEvent"),
+    TEST_RESPONSE_EVENT("testEventAnswer"),
+    ON_PASSWORD_CHANGE("onPasswordChange"),
+    ON_RESPONSE_PASSWORD_CHANGE("onPasswordChangeAnswer");
+}
---
+++ app/src/main/java/com/example/elorclass/socketIO/model/MessageOutput.kt
@@ -0,0 +1,3 @@
+package com.example.elorclass.socketIO.model
+
+data class MessageOutput (private val message: String)
