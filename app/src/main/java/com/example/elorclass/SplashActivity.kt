fbde161fc96936c89e6eb7368d9036cf2cc3e50c
Javi <in2dm3-v@elorrieta.int>
2025-02-04 15:55:57 +0100

    Eliminación de comentarios

--- app/src/main/java/com/example/elorclass/SplashActivity.kt
+++ app/src/main/java/com/example/elorclass/SplashActivity.kt
@@ -3,7 +3,6 @@ package com.example.elorclass
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
-import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 
 class SplashActivity: BaseActivity() {
@@ -13,17 +12,16 @@ class SplashActivity: BaseActivity() {
         setContentView(R.layout.activity_splash)
 
         val splashImage = findViewById<ImageView>(R.id.splash_image)
-        // Cargar el GIF con Glide en el ImageView
+
         Glide.with(this)
             .load(R.drawable.birrete)
             .into(splashImage)
 
-        // Esperar unos segundos antes de abrir la MainActivity
-        val splashScreenDuration: Long = 3000 // Duración del splash screen (3 segundos)
+        val splashScreenDuration: Long = 3000
         android.os.Handler().postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
-            finish() // Cerrar la SplashActivity para evitar volver atrás
+            finish()
         }, splashScreenDuration)
     }
 }
